5cead542a7942bdf7de2e5ea07634d485bcf766f
Vi Fendrick <violetynaf@gmail.com>
2023-08-29 22:08:29 -0500

    lol please

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (!someJobs.equals("")) {
+            System.out.print("No Results");
+            return;
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+            System.out.println("*****");
+            for (Map.Entry<String, String> entry : job.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println(key + ":" + " " + value);
+            }
+            System.out.println("*****\n");
+        }
+
     }
 }

92a34a2f6bf7f100f1ab4216b97d7e14856bdab0
Vi Fendrick <violetynaf@gmail.com>
2023-09-04 16:56:27 -0500

    done?

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -75,7 +75,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
@@ -94,8 +94,20 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+
+            for (String key : row.keySet()) {
+                String keyValue = row.get(key);
+
+                if (keyValue.toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
+                }
+            }
+        }
         // TODO - implement this method
-        return null;
+        return jobs;
     }
 
     /**
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -119,19 +116,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        if (!someJobs.equals("")) {
+        if (someJobs.isEmpty()) {
             System.out.print("No Results");
             return;
         }
 
+
         for (HashMap<String, String> job : someJobs) {
-            System.out.println("*****");
+            System.out.println("\n*****");
             for (Map.Entry<String, String> entry : job.entrySet()) {
                 String key = entry.getKey();
                 String value = entry.getValue();
                 System.out.println(key + ":" + " " + value);
             }
-            System.out.println("*****\n");
+            System.out.println("*****");
         }
 
     }
